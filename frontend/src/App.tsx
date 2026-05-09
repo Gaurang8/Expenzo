1e6096642eaa726ca99583b3e58417917bd02879
gaurang8 <gaurang8102003@gmail.com>
2026-05-09 18:45:17 +0530

    Initialize frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="text-2xl text-center my-[100px]">
+        Expanzo
+      </div>
+    </>
+  )
+}
+
+export default App
